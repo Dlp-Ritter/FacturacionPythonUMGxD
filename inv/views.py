035982630abe25b493e5fea01b2ae218bad70560
Dlp-Ritter <duglas_leonel_1704@protonmail.com>
2024-10-13 12:44:49 -0600

    subseccion

--- inv/views.py
+++ inv/views.py
@@ -44,3 +44,11 @@ class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         return super().form_valid(form)
+    
+class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
+    permission_required="inv.delete_categoria"
+    model=Categoria
+    template_name='inv/catalogos_del.html'
+    context_object_name='obj'
+    success_url=reverse_lazy("inv:categoria_list")
+    success_message="Categoría Eliminada Satisfactoriamente"
